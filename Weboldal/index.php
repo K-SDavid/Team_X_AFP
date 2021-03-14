285552b3eb59a56cc5a12a3c195d97cb702e1860
Koós Dávid <koos.david14@gmail.com>
2021-03-14 17:16:56 +0100

    style.css létrehozva
    Létrehoztam a style.css-t, majd az index.php-n belül belinkeltem.

--- Weboldal/index.php
+++ Weboldal/index.php
@@ -1,8 +1,11 @@
+<?php require_once 'protected/config.php'; ?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<title>Team-X</title>
+	<link rel="stylesheet" type="text/css" href="<?=PUBLIC_DIR.'style.css?'?>">
 </head>
 <body>
 	<header></header>
