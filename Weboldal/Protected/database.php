9abfdc434361acdba1b791ad2380a25a465f64ef
Koós Dávid <koos.david14@gmail.com>
2021-03-14 18:38:48 +0100

    database.php kiegészítve getConnection() functionnel.

--- Weboldal/Protected/database.php
+++ Weboldal/Protected/database.php
@@ -1,3 +1,8 @@
 <?php 
+function getConnection() {
+	$connection = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME.';',DB_USER, DB_PASS);
+	$connection->exec("SET NAMES '".DB_CHARSET."'");
+	return $connection;
+}
 
- ?>
+?>
