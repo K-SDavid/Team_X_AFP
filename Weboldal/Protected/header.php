0e67a87be5d25c0c29e9ce9d4b0c3d3a95bfed98
David <kissimond423@gmail.com>
2021-03-14 17:53:00 +0100

    header.php módosítása
    profil létrehozása

---
+++ Weboldal/Protected/header.php
@@ -0,0 +1,6 @@
+<div class="details">
+    <div class="money"><input type="text" name="money" value="€" readonly="true"></div>
+    <div class="xcoin"><input type="text" name="xcoin" value="X" readonly="true"></div>
+    <a href="index.php?P=profile"><div class="profile"><i class="fa fa-user"></i></div></a>
+</div>
+<hr>
