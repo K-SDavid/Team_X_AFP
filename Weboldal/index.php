1ffb3b8b777eedceabe06de070a4c0c03f1842ea
Koós Dávid <koos.david14@gmail.com>
2021-03-14 16:57:31 +0100

    index.php létrehozva

---
+++ Weboldal/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Team-X</title>
+</head>
+<body>
+	<header></header>
+	<nav></nav>
+	<content></content>
+	<footer></footer>
+</body>
+</html>
