1c7f340ed00c95118072a97db197d354cadeb34b
David <kissimond423@gmail.com>
2021-03-15 21:27:44 +0100

    usermanager.php UserLogout elkészült

--- Weboldal/Protected/usermanager.php
+++ Weboldal/Protected/usermanager.php
@@ -47,4 +47,10 @@ function UserLogin($username, $password) {
 function CheckLogin() {
 	return $_SESSION  != null && array_key_exists('uid', $_SESSION) && is_numeric($_SESSION['uid']);
 }
+
+function UserLogout() {
+	session_unset();
+	session_destroy();
+	header('Location: index.php');
+}
 ?>
