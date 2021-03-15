e8dcd15cce4a5a9bfded755196994aaee8d7b711
David <kissimond423@gmail.com>
2021-03-15 21:22:04 +0100

    usermanager.php UserLogin function megírva

--- Weboldal/Protected/usermanager.php
+++ Weboldal/Protected/usermanager.php
@@ -21,4 +21,26 @@ function UserRegister($username, $password, $age, $email) {
 	} 
 	return false;
 }
+
+function UserLogin($username, $password) {
+	$query = "SELECT id, username, password, age, email, permission, balance, xcoin FROM users WHERE username = :username AND password = :password";
+	$params = [
+		':username' => $username,
+		':password' => sha1($password)
+	]; 
+
+	require_once DATABASE_CONTROLLER;
+	$record = getRecord($query, $params);
+	if(!empty($record)) {
+		$_SESSION['uid'] = $record['id'];
+		$_SESSION['username'] = $record['username'];
+		$_SESSION['age'] = $record['age'];
+		$_SESSION['email'] = $record['email'];
+		$_SESSION['permission'] = $record['permission'];
+		$_SESSION['balance'] = $record['balance'];
+		$_SESSION['xcoin'] = $record['xcoin'];
+		header('Location: index.php?P=home');
+	}
+	return false;
+}
 ?>
