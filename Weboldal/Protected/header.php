81cab9302b70ffdffbe1481000275f3f6ec2814d
csacas7 <csacas7@gmail.com>
2021-03-15 21:23:15 +0100

    Header bővítése
    Register gomb hozzáadva a header-höz

--- Weboldal/Protected/header.php
+++ Weboldal/Protected/header.php
@@ -1,3 +1,7 @@
+<div class="register">
+	<a href="index.php?P=register">Regisztráció</a>
+</div>
+
 <div class="details">
     <div class="money"><input type="text" name="money" value="€" readonly="true"></div>
     <div class="xcoin"><input type="text" name="xcoin" value="X" readonly="true"></div>
