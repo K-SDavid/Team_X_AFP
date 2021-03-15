97915d8c479430db34a1ceb5bc1f22387f9133d7
csacas7 <csacas7@gmail.com>
2021-03-15 21:23:50 +0100

    Register input létrehozása

---
+++ Weboldal/Protected/user/register.php
@@ -0,0 +1,11 @@
+<form method="POST">
+	<input type="text" name="username" placeholder="Felhasználónév">
+	<input type="text" name="email" placeholder="Email">
+	<br>
+	<input type="text" name="age" placeholder="Életkor">
+	<br>
+	<input type="password" name="password" placeholder="Jelszó">
+	<input type="password" name="password2" placeholder="Jelszó megerősítő">
+	<br>
+	<input type="submit" name="submit" value="Regisztráció">
+</form>
