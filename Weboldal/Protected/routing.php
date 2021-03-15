7b8e703be68968b49a454393d597f9dca4f5e008
csacas7 <csacas7@gmail.com>
2021-03-15 21:23:36 +0100

    Routing bővítése
    Routing bővítése register-rel

--- Weboldal/Protected/routing.php
+++ Weboldal/Protected/routing.php
@@ -5,6 +5,8 @@ if(!array_key_exists('P', $_GET) || empty($_GET['P']))
 switch ($_GET['P']) {
 	case 'home': require_once PROTECTED_DIR.'normal/home.php'; break;
 
+	case 'register': require_once PROTECTED_DIR.'user/register.php'; break;
+
 	default: require_once PROTECTED_DIR.'normal/404.php'; break;
 }
 ?>
