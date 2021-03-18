cab3a406ba0006ef4b1c2d7b34e06860185cfedc
csacas7 <csacas7@gmail.com>
2021-03-18 19:15:55 +0100

    Elírás javítva

--- Weboldal/index.php
+++ Weboldal/index.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" type="text/css" href="<?=PUBLIC_DIR.'footerstyle.css?'?>">
 	<link rel="stylesheet" type="text/css" href="<?=PUBLIC_DIR.'navstyle.css?'?>">
 
-	<script type="text/javascript" src="script/togglenav.js"></script>
+	<script type="text/javascript" src="<?=PUBLIC_DIR.'script/togglenav.js'?>"></script>
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
