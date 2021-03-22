a0e415a52a1958392195bf8e5ea6d192f5885c80
Koós Dávid <koos.david14@gmail.com>
2021-03-22 13:04:55 +0100

    deposit.php kiegészítése
    Levizsgáltam, hogy van-e kártya csatolva a felhasználónak. Ki lehet választani, hogy melyik kártyával szeretne fizetni.

--- Weboldal/Protected/user/deposit.php
+++ Weboldal/Protected/user/deposit.php
@@ -1,3 +1,18 @@
 <?php 
+require_once CARD_MANAGER;
+$cards = ListCard($_SESSION['uid']);
+if(	!CheckCard($_SESSION['uid'])):
+	echo "Nincs kártya hozzáadva";
+else:
 
 ?>
+
+<div class="depositform">
+	Válassza ki melyik kártyával szeretne fizetni:	
+	<select name="amount">
+		<?php foreach($cards as $c): ?>
+			<option value="<?=$c['id']?>"><?=$c['cardnumber']?></option>
+		<?php endforeach; ?>
+	</select>
+</div>
+<?php  endif; ?> 
