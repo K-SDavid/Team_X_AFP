52466a0bbd6ce8a23f5cc90be7f6bee8eb36460d
Koós Dávid <koos.david14@gmail.com>
2021-03-27 17:56:41 +0100

    prizemanager.php kiegészítése RedeemPrize() functionnel

--- Weboldal/Protected/prizemanager.php
+++ Weboldal/Protected/prizemanager.php
@@ -6,4 +6,13 @@ function ListPrize(){
 	return getList($query);
 }
 
+function RedeemPrize($userid, $prizeid){
+	$query = "SELECT price FROM prizes WHERE id = :id";
+	$params = [ 'id' => $prizeid ];	
+	require_once DATABASE_CONTROLLER;
+	$price = getField($query, $params);
+	require_once USER_MANAGER;
+	SpendXcoin($userid, $price);
+} 
+
 ?>
