c1201ce153709151c92ba68809ae9f96e2a8472f
csacas7 <csacas7@gmail.com>
2021-03-29 00:17:36 +0200

    script megírása a registerhez

--- Weboldal/Public/script/showpw.js
+++ Weboldal/Public/script/showpw.js
@@ -1,11 +1,41 @@
-var state = false;
+var log = false;
 function showlpw(){
-	if(state){
+	if(log){
 		document.getElementById("lpassword").setAttribute("type", "password");
-		state = false;
+		document.getElementById("log").className = "fa fa-eye";
+		log = false;
 
 	} else{
 		document.getElementById("lpassword").setAttribute("type", "text");
-		state = true;
+		document.getElementById("log").className = "fa fa-eye-slash";
+		log = true;
+	}
+}
+
+var reg = false;
+function showrpw(){
+	if(reg){
+		document.getElementById("rpassword").setAttribute("type", "password");
+		document.getElementById("r1").className = "fa fa-eye";
+		reg = false;
+
+	} else{
+		document.getElementById("rpassword").setAttribute("type", "text");
+		document.getElementById("r1").className = "fa fa-eye-slash";
+		reg = true;
+	}
+}
+
+var reg2 = false;
+function showr2pw(){
+	if(reg2){
+		document.getElementById("r2password").setAttribute("type", "password");
+		document.getElementById("r2").className = "fa fa-eye";
+		reg2 = false;
+
+	} else{
+		document.getElementById("r2password").setAttribute("type", "text");
+		document.getElementById("r2").className = "fa fa-eye-slash";
+		reg2 = true;
 	}
 }
