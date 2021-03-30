ce0b1bbce67149e2c885a4392e63ceebc4b44a0f
David <kissimond423@gmail.com>
2021-03-30 21:28:27 +0200

    userlist.php kiegészítése bejelentkezés/permission vizsgálattal

--- Weboldal/Protected/admin/userlist.php
+++ Weboldal/Protected/admin/userlist.php
@@ -1,37 +1,41 @@
 <?php
-require_once USER_MANAGER;
-$users = UserList();
-if(count($users) > 0): ?>
-	<table>
-	<thead>
-		<tr>
-			<th>#</th>
-			<th>Felhasználónév</th>
-			<th>Email</th>
-			<th>Életkor</th>
-			<th>Egyenleg</th>
-			<th>X-Coin</th>
-			<th>Befizetett összeg</th>
-			<th>Kiváltott összeg</th>
-			<th>Jogosultság</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php foreach ($users as $u) : ?>
-			<tr>
-				<th><?=$u['id']?></th>
-				<td><?=$u['username']?></td>
-				<td><?=$u['email']?></td>
-				<td><?=$u['age']?></td>
-				<td><?=$u['balance']?></td>
-				<td><?=$u['xcoin']?></td>
-				<td><?=$u['deposit']?></td>
-				<td><?=$u['withdraw']?></td>
-				<td><?=$u['permission']?></td>
-			</tr>
-		<?php endforeach;?>
-	</tbody>
-</table>
-<?php else: ?>
-	<h1>Nem található regisztrált felhasználó!</h1>
-<?php endif; ?>
+	require_once USER_MANAGER;
+	if(!CheckLogin() || $_SESSION['permission'] < 3):
+		header("Location: index.php?P=denied");
+	else:
+		$users = UserList();
+		if(count($users) > 0): ?>
+			<table>
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Felhasználónév</th>
+					<th>Email</th>
+					<th>Életkor</th>
+					<th>Egyenleg</th>
+					<th>X-Coin</th>
+					<th>Befizetett összeg</th>
+					<th>Kiváltott összeg</th>
+					<th>Jogosultság</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php foreach ($users as $u) : ?>
+					<tr>
+						<th><?=$u['id']?></th>
+						<td><?=$u['username']?></td>
+						<td><?=$u['email']?></td>
+						<td><?=$u['age']?></td>
+						<td><?=$u['balance']?></td>
+						<td><?=$u['xcoin']?></td>
+						<td><?=$u['deposit']?></td>
+						<td><?=$u['withdraw']?></td>
+						<td><?=$u['permission']?></td>
+					</tr>
+				<?php endforeach;?>
+			</tbody>
+			</table>
+		<?php else: ?>
+			<h1>Nem található regisztrált felhasználó!</h1>
+		<?php endif; ?>
+	<?php endif; ?>
