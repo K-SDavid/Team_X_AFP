3fd629f9e0b070699d27dbb2562ccbed07ddc9f9
David <kissimond423@gmail.com>
2021-04-01 22:25:23 +0200

    submit.php elkészítve

---
+++ Weboldal/Protected/normal/submit.php
@@ -0,0 +1,19 @@
+<?php
+	function Submit() {
+		if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['yes'])) {
+			return 1;
+		}
+		if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['no'])) {
+			return 0;
+		}
+		return -1;
+	}
+	
+?>
+<form method="POST">
+	<label>Biztosan szeretné folytatni?</label>
+	<div>
+		<input type="submit" name="yes" value="Igen">
+		<input type="submit" name="no" value="Mégse">
+	</div>
+</form>
