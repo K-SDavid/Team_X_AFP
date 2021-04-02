ca918d69529ca4b84e1092ece0f6a3f4c1f013de
csacas7 <csacas7@gmail.com>
2021-04-02 11:15:25 +0200

    changePassword function megírása

--- Weboldal/Protected/usermanager.php
+++ Weboldal/Protected/usermanager.php
@@ -75,6 +75,20 @@ function ModifyUser($id, $balance, $xcoin) {
 	return false;
 }
 
+function changePassword($id, $password) {
+	$query = "UPDATE users SET password = :password WHERE id = :id";
+	$params = [
+		':id' => $id,
+		':password' => sha1($password)
+	];
+	require_once DATABASE_CONTROLLER;
+	if(executeDML($query, $params))
+		{
+			header('Location: index.php?P=profile');
+		}
+	return false;
+}
+
 function DeleteUser($id) {
 	$query = "DELETE FROM users WHERE id = :id";
 	$params = [ ':id' => $id ];
