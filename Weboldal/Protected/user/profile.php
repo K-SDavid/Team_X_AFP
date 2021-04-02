56ec36496820a4dff91528a2e41a70305a876df4
csacas7 <csacas7@gmail.com>
2021-04-02 11:15:52 +0200

    Jelszó változtatás php kódjának megírása

--- Weboldal/Protected/user/profile.php
+++ Weboldal/Protected/user/profile.php
@@ -1,4 +1,29 @@
-
+<?php 
+require_once USER_MANAGER;
+  if(!CheckLogin()):
+    header("Location: index.php?P=denied");
+  else:
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['npwsubmit'])) {
+      $postData = [
+        'id' => $_SESSION['uid'],
+        'npassword' => $_POST['npassword'],
+        'npassword1' => $_POST['npassword1']
+      ];
+      if(empty($postData['npassword']) || empty($postData['npassword1'])) {
+        echo "Kérem adja meg az új jelszavát vagy jelszó megerősítőt!";
+      } else if(strlen($postData['npassword']) < 6) {
+        echo "A jelszó túl rövid! Legalább 6 karakter hosszúnak kell lennie!";
+      } else if(1 === preg_match('~[ ]~', $postData['npassword'])) {
+        echo "A jelszó nem tartalmazhat szóközt!";
+      } else if ($postData['npassword'] != $postData['npassword1']) {
+        echo "A jelszavak nem egyeznek!";       
+      }else {
+        if(!changePassword($postData['id'], $postData['npassword'])) {
+          echo "Hiba a jelszó módosításnál!";
+        }
+      }
+    }
+?>
 
 <?php require_once PROTECTED_DIR."creditcard/listcard.php" ?>
 <hr width="100%">
@@ -11,8 +36,8 @@
 <div class="changepw">
   <form method="POST">
     <input type="password" name="npassword" placeholder="Adja meg az új jelszavát">
-    <input type="password" name="npassword" placeholder="Erősítse meg a jelszavát">
-    <input type="submit" name="npwsubbmit" value="Jelszó módosítás">
+    <input type="password" name="npassword1" placeholder="Erősítse meg a jelszavát">
+    <input type="submit" name="npwsubmit" value="Jelszó módosítás">
   </form>
 </div>
 <input type="submit" name="jelszomodositas" value="Jelszó változtatás" onclick="togglePW()">
@@ -37,3 +62,4 @@
     </tr>
   </table>
 </div>
+<?php endif; ?>
