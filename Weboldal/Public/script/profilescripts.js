919e7bd8201b9330ab594ed08feb98530164547a
csacas7 <csacas7@gmail.com>
2021-04-02 10:32:08 +0200

    Profilescripts.js bővítése addcard-dal

--- Weboldal/Public/script/profilescripts.js
+++ Weboldal/Public/script/profilescripts.js
@@ -2,3 +2,8 @@ function togglePW(){
 	let navigation = document.querySelector('.changepw')
 	navigation.classList.toggle('activepw')
 }
+
+function toggleaddcc(){
+	let navigation = document.querySelector('.addcc')
+	navigation.classList.toggle('activecc')
+}
