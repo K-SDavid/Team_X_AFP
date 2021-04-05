d715e7b42cddcdc25e894c13fc07536dd3dc78e8
Koós Dávid <koos.david14@gmail.com>
2021-04-05 16:23:14 +0200

    prizes.php bővítése, nyithatóak az xcoin ládák

--- Weboldal/Protected/user/prizes.php
+++ Weboldal/Protected/user/prizes.php
@@ -3,8 +3,9 @@ require_once USER_MANAGER;
 	if(!CheckLogin()):
 		header("Location: index.php?P=denied");	
 	else:
-		if(array_key_exists('c',$_GET) && !empty($_GET['c'])):	
-			$percent = [55,25,18,1.2,0.6,0.15,0.04,0.01];			
+		if(array_key_exists('c',$_GET) && !empty($_GET['c'])):				
+			$percent = [55,25,18,1.2,0.6,0.15,0.04,0.01];		
+			$success = false;	
 			switch ($_GET['c']) {
 				case 'bronze':
 					$amount = [0.5,1,1.6,20,50,250,1500,7500];
@@ -22,11 +23,31 @@ require_once USER_MANAGER;
 					$amount = [100,250,350,1250,8500,25000,95000,500000];
 					$price=4500;
 					break;
+				case 'open-bronze':
+					$amount = [0.5,1,1.6,20,50,250,1500,7500];
+					if($_SESSION['xcoin']>=50)					
+						$success = SpendXcoin($_SESSION['uid'],50);					
+					break;
+				case 'open-silver':
+					$amount = [3,5,10,150,500,1500,7500,32500];
+					$success = SpendXcoin($_SESSION['uid'],250);
+					break;
+				case 'open-gold':
+					$amount = [10,20,35,500,2000,9500,35000,125000];
+					$success = SpendXcoin($_SESSION['uid'],1000);					
+					break;
+				case 'open-diamond':
+					$amount = [100,250,350,1250,8500,25000,95000,500000];	
+					$success = SpendXcoin($_SESSION['uid'],4500);			
+					break;
 				default:
 					header("Location:index.php?P=prizes");
 					break;
-			}	
-			$openstring="index.php?P=prizes&c=open-{$_GET['c']}";	
+			}			
+			$openstring="index.php?P=prizes&c=open-{$_GET['c']}";
+			if(!$success & strlen($_GET['c'])>8){
+					header("Location:index.php?P=denied");
+			}
 			?>
 			<?php if(strlen($_GET['c'])<8):?>
 				<?php if($_SESSION['xcoin']<$price): ?>					
@@ -35,7 +56,6 @@ require_once USER_MANAGER;
 				<?php else: ?>					
 					<a href="<?=$openstring?>">Kinyitás</a>
 					<hr style="width:85%; margin-bottom: 30px; margin-top: 20px;">
-
 				<?php endif; ?>
 			<table >
 				<tbody>					
@@ -54,12 +74,16 @@ require_once USER_MANAGER;
 			<hr style="width:90%; margin: 30px;">
 			<h1>Nyerési esélyek</h1>
 		<?php else: ?>	
-			
+			<a href="index.php?P=prizes">Vissza a főoldalra</a>
+			<hr style="width:90%; margin: 30px;">		
+			<?php			
+				$number = (XcoinChestRoll());	
+				Win($_SESSION['uid'],$amount[$number]);
+				echo "Gratulálunk! Nyereménye:".$amount[$number]."€";				
+			?>
 		<?php endif; ?>
 		<hr style="width:90%; margin: 30px;">
 		<img src="<?=PUBLIC_DIR."Pictures/xcoin-chest/{$_GET['c']}-chest.png"?>">
-	
-
 	<?php else: ?>
 	 	<?php require_once PROTECTED_DIR."games/xcoin-chest/chest.php";?>
 		<h1>X-Coinból nyitható ládák</h1>
@@ -90,7 +114,6 @@ function XcoinChestRoll(){
 		default:
 			return 0;
 			break;
-	}
-	
+	}	
 }
  ?>
