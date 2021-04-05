9e4a5b0863cc5cd749e1664358a241b1ec89ded7
David <kissimond423@gmail.com>
2021-04-05 17:09:32 +0200

    prizemanager.php AddPrize() megírva

--- Weboldal/Protected/prizemanager.php
+++ Weboldal/Protected/prizemanager.php
@@ -1,4 +1,18 @@
 <?php  
+function AddPrize($name, $price) {
+	$query = "INSERT INTO prizes(name, price) VALUES (:name, :price)";
+	$params = [ 
+		':name' => $name,
+		':price' => $price
+	];
+	require_once DATABASE_CONTROLLER;
+	if(executeDML($query, $params))
+        {
+            header('Location: index.php?P=prizes');
+        }
+    } 
+    return false;
+}
 
 function ListPrize(){
 	$query = "SELECT * FROM prizes";
