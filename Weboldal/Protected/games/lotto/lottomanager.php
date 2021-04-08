c51d643e03379ef16b494c6650f9393b11558060
David <kissimond423@gmail.com>
2021-04-08 21:58:04 +0200

    lottomanager.php kiegészítése
    DeleteLotto()
    LottoRoll()
    CountLotto()
    Reward()
    DeleteAllLotto()

--- Weboldal/Protected/games/lotto/lottomanager.php
+++ Weboldal/Protected/games/lotto/lottomanager.php
@@ -23,4 +23,66 @@ function ListLotto() {
 	require_once DATABASE_CONTROLLER;
 	return getList($query);
 }
+
+function DeleteLotto($id) {
+	$query = "DELETE FROM lotto WHERE id = :id";
+	$params = [ ':id' => $id ];
+	require_once DATABASE_CONTROLLER;
+	if(executeDML($query, $params))
+	{
+		header('Location: index.php?P=listlotto');
+	}
+	return false;
+}
+
+function LottoRoll() {
+	$temp = range(1, 90);
+    shuffle($temp);
+    return array_slice($temp, 0, 5);
+}
+
+function CountLotto($s = [], $c) {
+	$list = ListLotto();
+	$temp = 0;
+	foreach ($list as $l) {
+		array_shift($l);
+		array_shift($l);
+		$result = array_intersect($l, $s);
+		if(count($result) == $c) {
+			$temp++;
+		}
+	}
+	return $temp;
+}
+
+function Reward($s = [], $hit1, $hit2, $hit3, $hit4, $hit5) {
+	$list = ListLotto();
+	$userid = 0;
+	foreach ($list as $l) {
+		$userid = $l['userid'];
+		array_shift($l);
+		array_shift($l);
+		$result = array_intersect($l, $s);
+		if(count($result) == 1) {
+			Win($userid, $hit1);
+		} else if (count($result) == 2) {
+			Win($userid, $hit2);
+		} else if (count($result) == 3) {
+			Win($userid, $hit3);
+		} else if (count($result) == 4) {
+			Win($userid, $hit4);
+		} else if (count($result) == 5) {
+			Win($userid, $hit5);
+		}
+	}
+	UpdateBalance($_SESSION['uid']);
+	DeleteAllLotto();
+	header('Location: index.php?P=lottoroll');
+}
+
+function DeleteAllLotto() {
+	$query = "TRUNCATE TABLE lotto";
+	require_once DATABASE_CONTROLLER;
+	executeDML($query);
+}
 ?>
