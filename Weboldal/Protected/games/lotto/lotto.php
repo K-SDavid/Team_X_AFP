27cfe5d93107e74dba84b9392802e765b6de7431
David <kissimond423@gmail.com>
2021-04-08 21:59:15 +0200

    lotto.php kiegészítése
    lottószámok listázás gomb
    lottószámok kihúzása gomb adminok részére

--- Weboldal/Protected/games/lotto/lotto.php
+++ Weboldal/Protected/games/lotto/lotto.php
@@ -1,13 +1,27 @@
 <?php
 require_once USER_MANAGER;
+require_once 'lottomanager.php';
 if (CheckLogin()):
 	if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['addlotto'])) {
 		header("Location: index.php?P=addlotto");
 	}
+	if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['listlotto'])) {
+		header("Location: index.php?P=listlotto");
+	}
+	if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['roll'])) {
+		header("Location: index.php?P=lottoroll");
+	}
 ?>
 <form method="POST">
 	<input type="submit" name="addlotto" value="Ötöslottó vásárlása">
+	<br>
+	<input type="submit" name="listlotto" value="Számok megtekintése">
+	<?php if ($_SESSION['permission'] > 2): ?>
+		<br>
+		<input type="submit" name="roll" value="Számok kihúzása">
+	<?php endif ?>
 </form>
 <?php endif;?>
+<p>Ár: 5€</p>
 <p>A játék lényege 5 szám kiválasztása 0 és 90 között, majd sorsoláskor 5 számot kihúznak, és a megegyező számok függvényében nyerhető jutalom.</p>
 <h1>Játékleírás:</h1>
