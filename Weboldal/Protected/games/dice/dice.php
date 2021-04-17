d5b2a8c2c9328b61fead7275b5e9c8c6d6efdb27
Koós Dávid <koos.david14@gmail.com>
2021-04-17 13:49:29 +0200

    dice.php kiegészítése checklogin-nal és fogadási lehetőségekkel

--- Weboldal/Protected/games/dice/dice.php
+++ Weboldal/Protected/games/dice/dice.php
@@ -1,3 +1,34 @@
+<?php
+require_once USER_MANAGER;
+if(!CheckLogin()): 
+  header("Location: index.php?P=denied");
+else:
+  ?>
+  <form method="POST" class="dicebet">
+    <hr class="nicehr">
+     <table>
+      <tr>
+        <td><label for="27">2-7</label></td>
+        <td><label for="712">7-12</label></td>
+        <td><label for="double">DUPLA</label></td>
+        <td><label for="odd">PÁRATLAN</label></td>
+        <td><label for="even">PÁROS</label></td>
+        <td><label for="exact"><input style="width:60px;"type="number" value="2" name="exactsum" placeholder="Pontos összeg" min="2" max="12"></label></td>
+      </tr>
+      <tr>
+        <td><input type="radio" name="dicebet" id="27" value="27"> </td>
+        <td><input type="radio" name="dicebet" id="712" value="712"> </td>
+        <td><input type="radio" name="dicebet" id="double" value="double"></td>
+        <td><input type="radio" name="dicebet" id="odd" value="odd"></td>
+        <td><input type="radio" name="dicebet" id="even" value="even"></td>
+        <td><input type="radio" name="dicebet" id="exact" value="exact"></td>
+      </tr>
+     </table>
+     <hr class="nicehr">
+   </form>
+<?php endif; ?>
+
+
 <div class="dice first-face">
   <span class="dot">
   </span>
