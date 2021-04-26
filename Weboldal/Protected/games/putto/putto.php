e93f94b4bae86e748f7fc346fefe4b68b406fc25
Koós Dávid <koos.david14@gmail.com>
2021-04-26 13:38:54 +0200

    putto.php-ban létrehoztam a mezőket a számoknak

--- Weboldal/Protected/games/putto/putto.php
+++ Weboldal/Protected/games/putto/putto.php
@@ -1,1 +1,21 @@
-<?php  ?>
+<div class="b-mezo">
+	<?php for ($i=1; $i < 5; $i++): ?> 
+		<input type="radio" name="bmezo"><?=$i?>		
+	<?php endfor;?>
+</div>
+<hr class="nicehr">
+<div class="a-mezo">
+	<table>
+		<?php 
+		$counter = 1;
+		for ($i=0; $i < 4; $i++): ?>
+			<tr>
+			<?php 
+			for ($j=0; $j < 5; $j++): ?>
+				<td><input type="checkbox" name="amezo"><?=$counter?></td>
+				<?php $counter++; 
+			endfor;?>
+			</tr>
+		<?php endfor; ?>
+	</table>
+</div>
