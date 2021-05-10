2fb84e380ecd0f0b2437cf6bc99eedc2c3d9d1bb
pmarcell <marcell.pelle@gmail.com>
2021-05-10 13:34:09 +0200

    fortune wheel alap css

---
+++ Weboldal/Protected/games/wheel/wheel.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style >
+    *{
+      box-sizing: border-box;
+      padding: 0;
+      margin: 0;
+      outline: none
+
+    }
+    body{
+      font-family: sans-serif;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      overflow: hidden;
+      background: #ffff;
+      background-size: cover;
+    }
+
+    </style>
+  </head>
+  <body>
+
+
+  </body>
+</html>
